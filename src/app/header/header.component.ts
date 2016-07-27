a4caa0a2abc29ee332d1b7071103afbd8532b001
Neil Young <neil.young@substanceglobal.com>
2016-07-27 13:06:39 +0100

    Latest changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'app-header',
+  directives: [ROUTER_DIRECTIVES],
   templateUrl: 'header.component.html',
   styleUrls: ['header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {
   }
 
   showAlert(event){
   	event.preventDefault();
-  	alert("Clicked!");
-
+  	alert("Sending you to races!");
+    this.navigateToRaces();
   	return false;
+  }
+
+  navigateToRaces(){
+    this.router.navigate(['/races']);
   }
 
 }
