795001a6ac6b4f42c15aafd572f8f9ba0993f531
Neil Young <neil.young@substanceglobal.com>
2016-07-27 12:08:24 +0100

    Adding template and scss for races component

--- src/app/races/races.component.ts
+++ src/app/races/races.component.ts
@@ -2,50 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { PolymerElement } from '@vaadin/angular2-polymer';
 
 @Component({
-    selector: 'app-races',
-    directives: [
-      PolymerElement('vaadin-combo-box'),
-      PolymerElement('paper-input'),
-      PolymerElement('google-youtube')
-    ],
-    styles: [
-      '.awesome-div{background-color:#F00}'
-    ],
-    template: `
-      <h2>Races</h2>
-      <button (click)="refreshRaces()">Refresh the races list</button>
-      <p>{{races.length}} races</p>
-      <ul>
-        <li *ngFor="let race of races; let i=index; let e=even">
-          {{ i }}: {{ race.name }} (Even? {{ e }})
-        </li>
-      </ul>
-      <textarea (keydown.alt.space)="onSpacePress($event)">Press space!</textarea>
-      <input type="text" #name>
-      {{ name.value }}
-      <button (click)="name.focus()">Focus the input</button>
-      <google-youtube
-        video-id="M7lc1UVf-VE"
-        height="270px"
-        width="480px"
-        rel="0"
-        start="5"
-        autoplay="0" #player>
-      </google-youtube>
-      <button (click)="player.pause()">Pause!</button>
-      <button (click)="player.play()">Play!</button>
-
-      <div [class.awesome-div]="isAnAwesomeDiv()">I've got style</div>
-` })
-  export class RacesComponent {
-    races: any = [];
-    refreshRaces() {
-      this.races = [{ name: 'London' }, { name: 'Lyon' }, { name: 'Califonia' }, { name: 'Krawkow' }, { name: 'Manchester' }];
-    }
-    onSpacePress(e){
-      console.log( e );
-    }
-    isAnAwesomeDiv(){
-      return true;
-    }
+  moduleId: module.id,
+  selector: 'app-races',
+  directives: [
+    PolymerElement('vaadin-combo-box'),
+    PolymerElement('paper-input'),
+    PolymerElement('google-youtube')
+  ],
+  templateUrl: 'races.component.html',
+  styleUrls: ['races.component.css']
+})
+export class RacesComponent {
+  races: any = [];
+  refreshRaces() {
+    this.races = [{ name: 'London' }, { name: 'Lyon' }, { name: 'Califonia' }, { name: 'Krawkow' }, { name: 'Manchester' }];
+  }
+  onSpacePress(e){
+    console.log( e );
+  }
+  isAnAwesomeDiv(){
+    return true;
+  }
 }
--- src/app/register-form/register-form.spec.ts
+++ src/app/register-form/register-form.spec.ts
@@ -5,6 +5,6 @@ import {RegisterForm} from './register-form';
 
 describe('RegisterForm', () => {
   it('should create an instance', () => {
-    expect(new RegisterForm()).toBeTruthy();
+    expect(new RegisterForm('', '', '')).toBeTruthy();
   });
 });
